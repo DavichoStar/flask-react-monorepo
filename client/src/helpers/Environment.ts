513e089d7e63aeca27db67db0d212d959022c7df
DavichoStar <erickdavid_estrella@hotmail.com>
2023-06-27 22:36:36 -0600

    Check env

--- client/src/helpers/Environment.ts
+++ client/src/helpers/Environment.ts
@@ -1,1 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:7000";
+if (!import.meta.env.VITE_API_URL)
+  throw new Error("[ENV] VITE_API_URL is not defined");
+
+export const API_URL = import.meta.env.VITE_API_URL;
